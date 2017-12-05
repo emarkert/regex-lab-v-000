8eec68f931971bd3ef530f8d10e7e539dd0c641b
Evan  Markert <evanmarkert@gmail.com>
2017-12-05 19:47:10 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,13 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[\.!?]$/)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+    true
+  else
+    false
+  end
 end
